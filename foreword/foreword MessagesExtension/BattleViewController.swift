2100fb1bacbaa44b7b83171aaca19a18b01f004a
julialwang <julialwa@cs.washington.edu>
2022-02-07 18:03:17 -0800

    this might break everything but all i tried to do was add one file

---
+++ foreword/foreword MessagesExtension/BattleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BattleViewController.swift
+//  foreword MessagesExtension
+//
+//  Created by Julia Wang on 2/7/22.
+//
+
+import Foundation
+import UIKit
+
+class BattleViewController: UIViewController {
+    
+    var battleTag:Int!
+    @IBOutlet weak var ibo_slotView:UIImageView!
+    var parentVC:MessagesViewController!
+    
+    var statusLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
